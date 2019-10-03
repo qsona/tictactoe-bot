6b4f986e3f183d3c834e2eeb7db70f634378c0da
qsona <mori.jmk@gmail.com>
2019-10-04 03:11:27 +0900

    Fix filename typo

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import start from './bot';
 
 import { TicTacToe } from './TicTacToe'
 
-import { TicTacToeCUIGame } from './TickTacToeCUIGame'
+import { TicTacToeCUIGame } from './TicTacToeCUIGame'
 import { registerGame } from './SlackGameManager'
 
 registerGame('tic-tac-toe', TicTacToe, TicTacToeCUIGame);
